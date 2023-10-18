07176651c2660c4c7ad61e3af4fc69d3ab95441d
Tom Callaghan <thomas.callaghan@ibsscorp.com>
2023-10-18 12:03:56 -0700

    Adding recruitment

--- SRC/ScallopPopDensity.f90
+++ SRC/ScallopPopDensity.f90
@@ -4,10 +4,11 @@ PROGRAM ScallopPopDensity
 
     use Growth_Mod
     use Data_Point_Mod
+    use Recruit_Mod
 
     implicit none
 
-    integer j  !> loop count
+    integer n, j  !> loop count
 
     character(2) domain_name
     integer start_year, stop_year
@@ -24,11 +25,13 @@ PROGRAM ScallopPopDensity
 
     type(Growth_Struct), allocatable :: growth(:)
     !type(MortalityStruct), allocatable :: mortality(:)
-    !type(RecruitStruct), allocatable :: recruit(:)
+    type(Recruit_Struct), allocatable :: recruit(:)
 
-    integer, parameter :: num_size_classes = 31 ! not 25, steps from 30 to 180 in steps of 5
+    integer, parameter :: num_size_classes = max_size_class ! not 25
 
-    real(dp), allocatable :: shell_height(:)
+    real(dp), allocatable :: state(:, :), shell_height(:)
+
+    real(dp) L30mm
 
 
     call Read_Input(domain_name, file_name, start_year, stop_year, fishing_type, num_time_steps, num_monte_carlo_iter)
@@ -57,7 +60,7 @@ PROGRAM ScallopPopDensity
     write(*,*) '========================================================'
 
     !!!allocate(growth(1:data_len) ,mortality(1:data_len) ,recruit(1:data_len) )
-    allocate(growth(1:data_len))
+    allocate(growth(1:data_len), recruit(1:data_len))
 
     do j=1, data_len
        growth(j)%region = domain_name
@@ -72,8 +75,58 @@ PROGRAM ScallopPopDensity
     !
     ! Assign recruitment by year and node
     !
-    !!!call SetRecruitment(recruit, data_len, domain_name, is_rand_recr)
+    call Set_Recruitment(recruit, data_len, domain_name, is_rand_recr)
     call Set_Growth(growth, grid, shell_height, delta_time, data_len, num_size_classes, domain_name)
     !!!call SetMortality(mortality, grid, shell_height, data_len, num_size_classes, domain_name)
 
+    !allocate(state(1:data_len,1:num_size_classes),StateTS(1:nts,1:num_size_classes),F(1:data_len),Samp(1:data_len, 1:num_size_classes),Wgrams(1:data_len,1:num_size_classes))
+    allocate(state(1:data_len,1:num_size_classes))
+
+    call Set_Initial_Conditions(data_len, num_size_classes, shell_height, start_year, domain_name, growth, state)
+    open(write_dev,file='RecIndx.txt')
+    do n = 1, data_len
+        L30mm = (growth(n) % L_inf_mu -30.D0) * exp(-growth(n) % K_mu)
+        do j=1, num_size_classes 
+            if (shell_height(j) .le. L30mm) recruit(n)%max_rec_ind = j
+        enddo
+        write(write_dev,*) n, L30mm, recruit(n)%max_rec_ind
+    enddo
+    close(write_dev)
+    
+
 END PROGRAM ScallopPopDensity
+
+subroutine Set_Initial_Conditions(data_len,num_size_classes,l,start_year,domain_name,growth,state)
+    use globals
+    use Growth_Mod
+    
+    implicit none
+    type(Growth_Struct), intent(in):: growth(*)
+    integer, intent(in):: data_len, num_size_classes, start_year
+    real(dp), intent(out):: state(data_len, *)
+    real(dp), intent(in):: l(*)
+    integer n, j, data_len_tmp, num_size_classes_tmp,tmp
+    character(2) domain_name
+    character(72) buffer, file_name
+    character(3) :: fType
+    
+    write(buffer,'(I6)')start_year
+    call Read_Input(domain_name,file_name,n,j,fType,tmp, data_len_tmp)
+    data_len_tmp=data_len
+    num_size_classes_tmp=num_size_classes
+    
+    write(*,*) file_name, domain_name
+    call Read_CSV(data_len_tmp, num_size_classes_tmp, file_name, state, data_len)
+    write(*,*) 'NSC ICS',num_size_classes_tmp, num_size_classes
+    do n=1,data_len
+        do j=num_size_classes,2,-1
+            if( (l(j).gt.growth(n)%L_inf_mu) .and. (state(n,j).gt.0.D0) )then
+                state(n,j-1)=state(n,j-1)+state(n,j)! lump scallops into smaller class
+                state(n,j)=0.D0
+            endif
+        enddo
+    enddo
+    
+    return
+endsubroutine Set_Initial_Conditions
+    
