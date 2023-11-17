b1354ee271510b5daad094b46edbd48c5fa36c0d
TCallaghan2 <149170121+TCallaghan2@users.noreply.github.com>
2023-11-17 12:40:26 -0700

    Cleanup

--- mfiles/SAMSgrid_interp.m
+++ mfiles/SAMSgrid_interp.m
@@ -20,31 +20,30 @@ for k=1:n
     f(k)=NaN;
     H(k,:)=NaN;
     end
-endfor
+end
 
 if(0)
-[ng,m]=size(fg);
-xe=mean(xg(E'));
-ye=mean(yg(E'));
-n=length(x);
-f=zeros(n,m);
-for k=1:n
-   [m,j]=min( abs(xe+i*ye - (x(k)+i*y(k)) ) );
-   [x1,i1]=min(xg(E(j,:)) );[x2,i2]=max(xg(E(j,:)) );
-   [y1,j1]=min(yg(E(j,:)) );[y2,j2]=max(yg(E(j,:)) );
-   fl=fg(E(j,:),:);
-   if inside(x(k),y(k),[x1,x2,x2,x1],[y1,y1,y2,y2])==1,
-    s=(x2-x1)*(y2-y1);
-    w1=[x2-x(k)]*[y2-y(k)];  
-    w2=[x(k)-x1]*[y2-y(k)];  
-    w4=[x2-x(k)]*[y(k)-y1];  
-    w3=[x(k)-x1]*[y(k)-y1]; 
-    f(k,:)=[w1*fl(1,:) + w2*fl(2,:)+ w3*fl(3,:)+ w4*fl(4,:)]/s; 
-%     f(k,:)=interp2(xg(E(j,:)),yg(E(j,:)),fg(E(j,:),:),x(k),y(k)); 
-        
-    else
-     f(k,:)=NaN;
-  endif
-endfor
-endif
-endfunction
+    [ng,m]=size(fg);
+    xe=mean(xg(E'));
+    ye=mean(yg(E'));
+    n=length(x);
+    f=zeros(n,m);
+    for k=1:n
+        [m,j]=min( abs(xe+i*ye - (x(k)+i*y(k)) ) );
+        [x1,i1]=min(xg(E(j,:)) );[x2,i2]=max(xg(E(j,:)) );
+        [y1,j1]=min(yg(E(j,:)) );[y2,j2]=max(yg(E(j,:)) );
+        fl=fg(E(j,:),:);
+        if inside(x(k),y(k),[x1,x2,x2,x1],[y1,y1,y2,y2])==1
+            s=(x2-x1)*(y2-y1);
+            w1=(x2-x(k))*(y2-y(k));  
+            w2=(x(k)-x1)*(y2-y(k));  
+            w4=(x2-x(k))*(y(k)-y1);  
+            w3=(x(k)-x1)*(y(k)-y1); 
+            f(k,:)=(w1*fl(1,:) + w2*fl(2,:)+ w3*fl(3,:)+ w4*fl(4,:))/s; 
+            % f(k,:)=interp2(xg(E(j,:)),yg(E(j,:)),fg(E(j,:),:),x(k),y(k)); 
+        else
+            f(k,:)=NaN;
+        end
+    end
+end
+end
